04337d9a3dfd8d404e8b513316e8b8ff8c69a78e
April <aprilcr87@gmail.com>
2021-12-21 15:28:09 -0600

    added background

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Welcome to Space Cadet!</Text>
+      <ImageBackground source={require('./assets/history-in-hd-unsplash.jpg')} resizeMode="cover" style={styles.image}>
+        
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +17,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    position: 'absolute',
+    zIndex: 1,
+    top: 50
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    height: '100%',
+    width: '100%'
+  }
 });
