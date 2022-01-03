79ce24d6200505ed2731eea23a8f78355fd624f3
April <aprilcr87@gmail.com>
2022-01-03 11:57:35 -0600

    finished apod with api call

--- screens/Apod.js
+++ screens/Apod.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Card, Text } from 'react-native-elements';
-import { useSelector } from 'react-redux';
-import { apod } from '../data/mockData';
+import { apodUrl } from '../service';
 
 const Apod = () => {
+    const [apodInfo, setApodInfo] = useState({});
+
+    useEffect(() => {
+        async function fetchApod() {
+            let response = await fetch(apodUrl, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  }
+            });
+            response = await response.json();
+            setApodInfo(response);
+          }
+      
+          fetchApod();
+      }, []);
 
     return(
         <View style={styles.screen}>
-            {apod ? <Card>
-              <Card.Title>{apod.title}</Card.Title>
-              <Text>{apod.date}</Text>
+            {apodInfo ? <Card>
+              <Card.Title>{apodInfo.title}</Card.Title>
+              <Text>{apodInfo.date}</Text>
               <Card.Divider />
               <Card.Image
                 style={{ padding: 0, height: 200, resizeMode: 'contain' }}
-                source={{ uri: apod.url }}
+                source={{ uri: apodInfo.url }}
               />
               <Text style={{ marginBottom: 10 }}>
-                {apod.explanation}
+                {apodInfo.explanation}
               </Text>
-              <Text>{apod.copyright}</Text>
+              <Text>{apodInfo.copyright}</Text>
           </Card> : ''}
         </View>
     );
